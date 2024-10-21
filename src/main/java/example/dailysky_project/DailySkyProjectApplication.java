b53e1ffea7cc0107d56203eb52121dc3dfbf4d8b
winy3ori <winy3ori@gmail.com>
2024-10-22 01:54:34 +0900

    first

---
+++ src/main/java/example/dailysky_project/DailySkyProjectApplication.java
@@ -0,0 +1,13 @@
+package example.dailysky_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailySkyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailySkyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/example/dailysky_project/DailySkyProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package example.dailysky_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class DailySkyProjectApplicationTests {
+
+    @Test
+    void testCode(){
+        assertEquals(1, 1);
+    }
+
+}
